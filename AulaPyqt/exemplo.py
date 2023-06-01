929335f3b6314246e9ab4cd4cdb8ade1b2724e02
Felipe Pereira <70305395+Flepsz@users.noreply.github.com>
2023-06-01 18:40:14 +0000

    Updt

--- AulaPyqt/exemplo.py
+++ AulaPyqt/exemplo.py
@@ -2,7 +2,6 @@ from PyQt5 import uic, QtGui
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QPushButton
 
-
 class telas():
     def __init__(self):
         app = QtWidgets.QApplication([])
