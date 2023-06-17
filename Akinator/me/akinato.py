950a2858a3e57bf9edbaf3c76cc29ea077012365
Felipe Pereira <luisfelipelfsp3@gmail.com>
2023-06-17 20:46:48 -0300

    Updt

--- Akinator/me/akinato.py
+++ Akinator/me/akinato.py
@@ -1,7 +1,11 @@
 import asyncio
 import requests
 from PyQt5 import QtWidgets, uic, QtGui, QtCore
-from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout
+import sys
+import random
 
 from akinator import (
     CantGoBackAnyFurther,
@@ -23,11 +27,6 @@ class AkinatorWorker(QtCore.QThread):
         self.answer = None
         self.back_button_pressed = False
 
-    def while_thread(self, func):
-            self.timer = QTimer()
-            self.timer.start(1)
-            self.timer.timeout.connect(func)
-
     def run(self):
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
@@ -42,28 +41,28 @@ class AkinatorWorker(QtCore.QThread):
         first_question = await self.aki.start_game()
         self.update_question_signal.emit(first_question)
 
-        def testgame():
-            if self.aki.progression <= 80:
-                if self.back_button_pressed:
-                    self.back_button_pressed = False
+        while self.aki.progression <= 80:
+            if self.back_button_pressed:
+                self.back_button_pressed = False
+                try:
+                    await self.aki.back()
+                    print('went back 1 question')
+                except CantGoBackAnyFurther:
+                    print('cannot go back any further!')
+            else:
+                if self.answer is not None:
                     try:
-                        print('went back 1 question')
-                    except CantGoBackAnyFurther:
-                        print('cannot go back any further!')
-                else:
-                    if self.answer is not None:
-                        try:
-                            answer = Answer.from_str(self.answer)
-                        except InvalidAnswer:
-                            print('Invalid answer')
-                        else:
-                            await self.aki.answer(answer)
-                        self.answer = None
+                        answer = Answer.from_str(self.answer)
+                    except InvalidAnswer:
+                        print('Invalid answer')
+                    else:
+                        await self.aki.answer(answer)
+                    self.answer = None
 
-                next_question = self.aki.question
-                self.update_question_signal.emit(next_question)
+            next_question = self.aki.question
+            self.update_question_signal.emit(next_question)
 
-        self.while_thread(testgame)
+            await asyncio.sleep(0.1)
 
         first_guess = await self.aki.win()
         if first_guess:
@@ -88,6 +87,15 @@ class AkinatorGUI:
         self.tela_principal = uic.loadUi("./telas/Main.ui")
         self.tela_finish = uic.loadUi("./telas/Finish.ui")
         self.tela_inicial.show()
+        self.lbAki = self.tela_principal.lbAki
+
+        self.image_paths = [
+            "./telas/akinatorimg/aki01.png",
+            "./telas/akinatorimg/aki02.png",
+            "./telas/akinatorimg/aki03.png",
+            "./telas/akinatorimg/aki04.png",
+            "./telas/akinatorimg/aki05.png",
+        ]
 
         self.tela_inicial.btnPlay.clicked.connect(self.play_game)
 
@@ -103,6 +111,18 @@ class AkinatorGUI:
         self.timer.timeout.connect(self.process_events)
         app.exec()
 
+    def change_image(self):
+        # Seleciona aleatoriamente um caminho de imagem da lista
+        random_image_path = random.choice(self.image_paths)
+
+        # Carrega a imagem e define-a no QLabel
+        pixmap = QPixmap(random_image_path)
+        self.lbAki.setPixmap(pixmap.scaled(
+            self.lbAki.size(),
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation
+        ))
+
     def play_game(self):
         self.tela_inicial.hide()
         self.tela_principal.show()
@@ -112,24 +132,30 @@ class AkinatorGUI:
         self.worker.show_finish_screen_signal.connect(self.show_finish_screen)
         self.worker.finished.connect(self.worker_finished)
         self.worker.start()
-
+        self.timer.start(100)
 
     def handle_yes(self):
+        self.change_image()
         self.worker.set_answer('yes')
 
     def handle_no(self):
+        self.change_image()
         self.worker.set_answer('no')
 
     def handle_idk(self):
+        self.change_image()
         self.worker.set_answer('idk')
 
     def handle_ps(self):
+        self.change_image()
         self.worker.set_answer('probably')
 
     def handle_pn(self):
+        self.change_image()
         self.worker.set_answer('probably not')
 
     def handle_bk(self):
+        self.change_image()
         self.worker.set_back_button_pressed()
 
     def update_question(self, question):
@@ -146,6 +172,7 @@ class AkinatorGUI:
 
     def worker_finished(self):
         self.worker = None
+        self.timer.stop()
 
     def process_events(self):
         QtCore.QCoreApplication.processEvents()
